928db54b602123f841d5f0311c721f38116fb854
Philipp Seitz <philipp.seitz@quantinuum.com>
2025-09-24 11:24:58 +0900

    Add missing $

--- workers/tkr_sqcsub/main.sh
+++ workers/tkr_sqcsub/main.sh
@@ -6,7 +6,7 @@ input_iformat_value=$(cat $input_iformat_file)
 input_oformat_value=$(cat $input_oformat_file)
 input_qpu_value=$(cat $input_qpu_file)
 
-. /vol0300/share/ra010014/jhpcq/x86/scripts/setenv-sqcsub.sh input_qpu_value
+. /vol0300/share/ra010014/jhpcq/x86/scripts/setenv-sqcsub.sh $input_qpu_value
 
 sqcsub --nqubits $input_nqubits_value \
     --nshots $input_nshots_value \
