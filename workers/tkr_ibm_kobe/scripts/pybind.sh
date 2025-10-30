093930aeb2552f7a71f9be06c82f50fe762ef8b3
Matthew Burke <matthew.burke@quantinuum.com>
2025-10-30 15:21:51 +0000

    Update pybind.sh

--- workers/tkr_ibm_kobe/scripts/pybind.sh
+++ workers/tkr_ibm_kobe/scripts/pybind.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-uv run g++ -O3 -Wall -undefined dynamic_lookup -shared -std=c++11 -fPIC $(uv run python -m pybind11 --includes) tkr_sqcsub.cpp -o example$(uv run python -m pybind11 --extension-suffix) ${SQC_COMPILE_OPTIONS}
+echo ${SQC_COMPILE_OPTIONS}
+uv run g++ -O3 -Wall -undefined dynamic_lookup -shared -std=c++11 -fPIC $(uv run python -m pybind11 --includes) tkr_sqcsub.cpp -o tkr_sqcsub$(uv run python -m pybind11 --extension-suffix) ${SQC_COMPILE_OPTIONS}
