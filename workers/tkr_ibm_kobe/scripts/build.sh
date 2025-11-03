1057536b48c50ee8fd6d4906712d90de31f155a0
Matthew Burke <matthew.burke@quantinuum.com>
2025-11-03 20:55:13 +0000

    Revert script files

--- workers/tkr_ibm_kobe/scripts/build.sh
+++ workers/tkr_ibm_kobe/scripts/build.sh
@@ -3,5 +3,4 @@ set -euo pipefail
 
 source "$( dirname -- "${BASH_SOURCE[0]}" )"/backend_setup.sh ibm-kobe-dacc
 gcc "$( dirname -- "${BASH_SOURCE[0]}" )"/../src/transpile_info.c -o build/transpile_info.o ${SQC_COMPILE_OPTIONS}
-gcc "$( dirname -- "${BASH_SOURCE[0]}" )"/../src/submit.c -o build/submit.o ${SQC_COMPILE_OPTIONS} 
 
