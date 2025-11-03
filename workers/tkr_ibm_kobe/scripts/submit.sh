9f1190ac47043e215dde6fd81b7ec12419eaffff
Matthew Burke <matthew.burke@quantinuum.com>
2025-11-03 14:46:33 +0000

    Do not reassign env vars

--- workers/tkr_ibm_kobe/scripts/submit.sh
+++ workers/tkr_ibm_kobe/scripts/submit.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-input_nqubits_value=$(cat $input_nqubits_file)
-input_nshots_value=$(cat $input_nshots_file)
-input_iformat_value=$(cat $input_iformat_file)
-input_oformat_value=$(cat $input_oformat_file)
-input_qpu_value=$(cat $input_qpu_file)
-
 . /vol0300/share/ra010014/jhpcq/x86/scripts/setenv-sqcsub.sh input_qpu_value
 
 sqcsub --nqubits $input_nqubits_value \
